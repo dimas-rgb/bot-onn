7ed1f31c77483da72571a2850ec3b6c1a752fde7
dimas-rgb <78903714+dimas-rgb@users.noreply.github.com>
2021-02-17 15:20:13 +0700

    Add files via upload

---
+++ aebot/donasi.js
@@ -0,0 +1,23 @@
+const donasi = (Ig, ---- => { 
+	return 
+	 `     
+             𝗗𝗢𝗡𝗔𝗦𝗜  
+
+     *DONASI SEIKHLASNYA:)* 
+ 
+  *BOT BY ${Dimas}*
+
+
+
+
+Note:
+Jika Gamau Donasi Setidaknya Follow IG Ngab:D
+Kalo Dah Donasi Silahkan Invit Di Group Kalian:)
+Makasih:)
+
+${Ig}
+
+     `
+}
+
+exports.donasi = donasi
